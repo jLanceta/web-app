a61489a7a50185c403c2c1aec0c7a441a09ee22d
Javi Lanceta <jlancetasalas@uma.es>
2022-10-17 17:05:31 +0200

    Primer commit El primer commit con los archivos para la pagina web.

---
+++ script.js
@@ -0,0 +1,48 @@
+const btn = document.querySelector('.boton')
+const div1 = document.querySelector('.div1')
+const div2 = document.querySelector('.div2')
+const div3 = document.querySelector('.div3')
+
+window.onload = () => {
+    // setTimeout(() => {
+        div1.style.right = '90%'
+        div2.style.right = '70%'
+        div3.style.right = '50%'        
+
+        div1.style.opacity = '1'
+        div2.style.opacity = '1'
+        div3.style.opacity = '1'
+
+        btn.style.left = '50%'
+        btn.style.opacity = 1
+
+        /*let rect = div2.getBoundingClientRect();
+        console.log(rect.top, rect.right, rect.bottom, rect.left);*/
+    // }, 500)
+}
+
+// document.addEventListener('mousemove', (event) => {
+//     //console.log(event.movementX)
+
+//     if (event.x >= window.innerWidth/2) {
+//         div1.style.left = '20%'
+//         div2.style.left = '30%'
+//         div3.style.left = '40%'
+//     } else {
+//         div1.style.left = '10%'
+//         div2.style.left = '20%'
+//         div3.style.left = '30%'
+//     }
+    
+//     /*if (event.movementX >= 7) {
+//         //div1.style.backgroundColor = "aqua"
+//         div1.style.left = '20%'
+//         div2.style.left = '30%'
+//         div3.style.left = '40%'
+//     } else if (event.movementY <= -7) {
+//         div1.style.backgroundColor = "red"
+//         div1.style.left = '10%'
+//         div2.style.left = '20%'
+//         div3.style.left = '30%'
+//     }*/
+// })
